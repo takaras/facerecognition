1feb2a1dc861e548226ca18f9099d2fb5917b00d
Takaras <ges.dim@gmail.com>
2020-03-14 21:33:06 +0100

    Navigation bar CSS styles

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Logo from '../Logo/Logo';
+import './Navigation.css';
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
   if (isSignedIn) {
     return (
-      <nav style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <nav>
         <Logo />
         <p
           onClick={() => onRouteChange('signin')}
@@ -16,9 +17,9 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
     );
   } else {
     return (
-      <nav style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <nav>
         <Logo />
-        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <div className="nav-bar">
           <p
             onClick={() => onRouteChange('signin')}
             className="f3 link dim black underline pa3 pointer"
